ccd026aae045ce3f3532eeb22700cc8dda25336b
anshulsharma0424 <anshulsharma0424@gmail.com>
2025-07-02 20:33:46 +0530

    Binary Search

---
+++ src/S02_BinarySearch/Q03_SearchInsertPosition.java
@@ -0,0 +1,24 @@
+package S02_BinarySearch;
+
+public class Q03_SearchInsertPosition {
+
+    public static int insertPosition(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
+        int ans = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if(arr[mid] >= target){
+                ans = mid;
+                high = mid - 1;
+            }
+            else low = mid + 1;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 4, 7 };
+        int toInsert = 6;
+        System.out.println(insertPosition(arr, toInsert));
+    }
+}
