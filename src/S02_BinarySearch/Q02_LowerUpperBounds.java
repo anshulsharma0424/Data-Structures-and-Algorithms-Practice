dabb9a642e5a1df786f69a434a18eb6997c01cc7
anshulsharma0424 <anshulsharma0424@gmail.com>
2025-07-02 20:14:19 +0530

    Binary Search

---
+++ src/S02_BinarySearch/Q02_LowerUpperBounds.java
@@ -0,0 +1,41 @@
+package S02_BinarySearch;
+
+public class Q02_LowerUpperBounds {
+
+    public static int lowerBound(int[] arr, int x){
+        int n = arr.length;
+        int ans = -1;
+        int low = 0, high = n-1;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(arr[mid] >= x){
+                ans = mid;
+                high = mid - 1;
+            }
+            else low = mid + 1;
+        }
+        return ans;
+    }
+
+    public static int upperBound(int[] arr, int x){
+        int n = arr.length;
+        int ans = -1;
+        int low = 0, high = n-1;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(arr[mid] > x){
+                ans = mid;
+                high = mid - 1;
+            }
+            else low = mid + 1;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 3, 5, 8, 9, 15, 19 };
+        int x = 9;
+        System.out.println(lowerBound(arr, x));
+        System.out.println(upperBound(arr, x));
+    }
+}
