b72b865b035b390d7766e0d1c924f9e48d7ba3cd
anshulsharma0424 <anshulsharma0424@gmail.com>
2025-07-09 12:09:44 +0530

    Linked List

---
+++ src/S03_LinkedList/Q04_ReverseList.java
@@ -0,0 +1,26 @@
+package S03_LinkedList;
+
+import static S03_LinkedList.Q01_Introduction.createLinkedList;
+import static S03_LinkedList.Q01_Introduction.printList;
+
+public class Q04_ReverseList {
+
+    public static Node reverseList(Node head) {
+        Node prev = null;
+        Node temp = head;
+        while (temp != null) {
+            Node next = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = next;
+        }
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Node head = createLinkedList(arr);
+        Node newHead = reverseList(head);
+        printList(newHead);
+    }
+}
