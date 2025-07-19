c17e531187dfb95a0611712a77cae2d915f2bfa7
anshulsharma0424 <anshulsharma0424@gmail.com>
2025-07-19 20:13:54 +0530

    Recursion

--- src/S04_Recursion/Q05_RecursionOnSubSequences.java
+++ src/S04_Recursion/Q05_RecursionOnSubSequences.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Q05_RecursionOnSubSequences {
     public static void printAllSubSequences(int idx, int[] arr, int n, ArrayList<Integer> list) {
-        if (idx >= arr.length) {
+        if (idx >= n) {
             System.out.println(list);
             return;
         }
@@ -15,7 +15,7 @@ public class Q05_RecursionOnSubSequences {
     }
 
     public static void printAllSubSequencesWhoseSumEqualsK(int idx, int[] arr, int n, ArrayList<Integer> list, int sum, int k) {
-        if (idx >= arr.length) {
+        if (idx >= n) {
             if (sum == k) {
                 System.out.println(list);
             }
@@ -30,7 +30,7 @@ public class Q05_RecursionOnSubSequences {
     public static void main(String[] args) {
         int[] arr = {1,2,1};
         ArrayList<Integer> list = new ArrayList<>();
-//        printAllSubSequences(0, arr, arr.length, list);
+        printAllSubSequences(0, arr, arr.length, list);
         printAllSubSequencesWhoseSumEqualsK(0, arr, arr.length, list, 0, 1);
     }
 }
