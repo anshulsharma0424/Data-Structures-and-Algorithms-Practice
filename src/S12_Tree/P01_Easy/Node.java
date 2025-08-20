2d4241f9ac477ddf1a7a4805444ab3ba738d2ded
anshulsharma0424 <anshulsharma0424@gmail.com>
2025-08-20 12:28:58 +0530

    Learning Binary Tree

---
+++ src/S12_Tree/P01_Easy/Node.java
@@ -0,0 +1,11 @@
+package S12_Tree.P01_Easy;
+
+public class Node {
+    int val;
+    Node left = null;
+    Node right = null;
+
+    public Node(int val) {
+        this.val = val;
+    }
+}
---
+++ src/S12_Tree/P01_Easy/Q01_Implementation.java
@@ -0,0 +1,47 @@
+package S12_Tree.P01_Easy;
+
+public class Q01_Implementation {
+
+    public static void inOrder(Node root) {
+        if (root == null) return;
+
+        inOrder(root.left);
+        System.out.print(root.val + " ");
+        inOrder(root.right);
+    }
+
+    public static void preOrder(Node root) {
+        if (root == null) return;
+
+        System.out.print(root.val + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public static void postOrder(Node root) {
+        if (root == null) return;
+
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.val + " ");
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        preOrder(root);
+        System.out.println();
+
+        inOrder(root);
+        System.out.println();
+
+        postOrder(root);
+        System.out.println();
+    }
+}
