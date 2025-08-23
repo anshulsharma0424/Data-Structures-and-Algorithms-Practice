cd4eec9dd58bb699828e4a2884edf3e2b9c71f5f
anshulsharma0424 <anshulsharma0424@gmail.com>
2025-08-23 19:26:50 +0530

    Learning Binary Tree

--- src/S12_Tree/P01_Easy/Q01_Implementation.java
+++ src/S12_Tree/P01_Easy/Q01_Implementation.java
@@ -1,9 +1,12 @@
 package S12_Tree.P01_Easy;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class Q01_Implementation {
 
+    // Tree Traversals - DFS
     public static void inOrder(Node root) {
         if (root == null) return;
 
@@ -28,11 +31,13 @@ public class Q01_Implementation {
         System.out.print(root.val + " ");
     }
 
+    // Height of a Binary Tree
     public static int heightOfTree(Node root) {
         if (root == null || (root.left == null && root.right == null)) return 0;
         return Math.max(heightOfTree(root.left), heightOfTree(root.right)) + 1;
     }
 
+    // Print Nth Level
     public static void printNthLevel(Node root, int n){
         if (root == null) return;
 
@@ -44,6 +49,7 @@ public class Q01_Implementation {
         printNthLevel(root.right, n-1);
     }
 
+    // Tree traversal - BFS
     public static void leverOrder(Node root){
         int level = heightOfTree(root) + 1;
         for (int i=1; i<=level; i++) printNthLevel(root, i);
@@ -53,7 +59,7 @@ public class Q01_Implementation {
         Queue<Node> q = new LinkedList<>();
         if (root != null) q.add(root);
 
-        while (q.size() > 0){
+        while (!q.isEmpty()){
             Node temp = q.peek();
             if (temp.left != null) q.add(temp.left);
             if (temp.right != null) q.add(temp.right);
@@ -62,26 +68,31 @@ public class Q01_Implementation {
         }
     }
 
+    // Size if Binary Tree - Total number of nodes in a binary tree
     public static int sizeOfBinaryTree(Node root) {
         if (root == null) return 0;
         return 1 + sizeOfBinaryTree(root.left) + sizeOfBinaryTree(root.right);
     }
 
+    // Sum of all nodes in a Binary Tree
     public static int sumOfTreeNodes(Node  root) {
         if (root == null) return 0;
         return root.val + sumOfTreeNodes(root.left) + sumOfTreeNodes(root.right);
     }
 
+    // Maximum node in a Binary Tree
     public static int maximumNode(Node root) {
         if (root == null) return Integer.MIN_VALUE;
         return Math.max(root.val, Math.max(maximumNode(root.left), maximumNode(root.right)));
     }
 
+    // Minimum node in a Binary Tree
     public static int minimumNode(Node root) {
         if (root == null) return Integer.MAX_VALUE;
         return Math.min(root.val, Math.min(minimumNode(root.left), minimumNode(root.right)));
     }
 
+    // Diameter of a Binary Tree - longest path between any two nodes in a tree (need not be paas through root node)
     public static int diameterOfBinaryTree(Node root){
         if (root == null) return 0;
         if (root.left == null && root.right == null) return 0;
@@ -95,6 +106,7 @@ public class Q01_Implementation {
         return Math.max(mid, Math.max(left, right));
     }
 
+    // Balanced Binary Tree - difference between left and right side from each node is at-most 1
     public static boolean isBalancedBinaryTree(Node root) {
         if (root == null) return true;
 
@@ -110,11 +122,57 @@ public class Q01_Implementation {
         return (isBalancedBinaryTree(root.left) && isBalancedBinaryTree(root.right));
     }
 
+    // Same trees - values are same with same structure
     public static boolean areSameBinaryTrees(Node root1, Node root2) {
         if (root1 == null && root2 == null) return true;
         if (root1 == null || root2 == null) return false;
         if (root1.val != root2.val) return false;
         return (areSameBinaryTrees(root1.left, root2.left) && areSameBinaryTrees(root1.right, root2.right));
+    }
+
+    // Binary Tree paths - from root to leaf nodes
+    public static List<String> binaryTreePaths(Node root){
+        List<String> ans = new ArrayList<>();
+        helper(root, ans, "");
+        return ans;
+    }
+
+    public static void helper(Node root, List<String> ans, String s){
+        if (root == null) return;
+        if (root.left == null && root.right == null) {
+            s += root.val;
+            ans.add(s);
+            return;
+        }
+        helper(root.left, ans, s+root.val + "->");
+        helper(root.right, ans, s+root.val + "->");
+    }
+
+    // Binary Tree paths - from any node to root node
+//    public static List<String> binaryTreePaths2(Node root) {
+//        List<String> ans = new ArrayList<>();
+//    }
+
+
+    // Lowest Common Ancestor (LCA)
+    public static Node lowestCommonAncestor(Node root, Node p, Node q){
+        if (p == root || q == root) return root;
+        if (p == q) return p;
+
+        boolean leftP = contains(root.left, p);
+        boolean rightQ = contains(root.right, q);
+
+        if (leftP && rightQ) return root;
+        if (!leftP && !rightQ) return root;
+        if (leftP && !rightQ) return lowestCommonAncestor(root.left, p, q);
+        if (!leftP && rightQ) return lowestCommonAncestor(root.right, p, q);
+        return root;
+    }
+
+    public static boolean contains(Node root, Node node){
+        if (root == null) return false;
+        if (root == node) return true;
+        return contains(root.left, node) || contains(root.right, node);
     }
 
     public static void main(String[] args) {
@@ -125,8 +183,9 @@ public class Q01_Implementation {
         root.left.right = new Node(5);
         root.right.left = new Node(6);
         root.right.right = new Node(7);
-//        root.right.left.left = new Node(8);
-//        root.right.left.left.left = new Node(18);
+        // root.left.right.left = new Node(8);
+        // root.right.left.left = new Node(8);
+        // root.right.left.left.left = new Node(18);
 
         // New Binary Trees
         // Tree-1
@@ -178,5 +237,9 @@ public class Q01_Implementation {
         System.out.println("Balanced BinaryTree: " + isBalancedBinaryTree(root));
 
         System.out.println("Are same Binary trees: " + areSameBinaryTrees(newRoot, newRoot2));
+
+        System.out.println(binaryTreePaths(root));
+
+        System.out.println("Lowest Common Ancestor: " + lowestCommonAncestor(root, root.left.left, root.left.right.left).val);
     }
 }
